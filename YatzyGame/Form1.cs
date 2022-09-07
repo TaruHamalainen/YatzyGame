4e0868f49a20a9e81891d0a1ee05170fc7e5a820
Taru Hämäläinen <82812247+TaruHamalainen@users.noreply.github.com>
2022-09-07 14:24:06 +0300

    dice class and setting image to picturebox when rolling dice

--- YatzyGame/Form1.cs
+++ YatzyGame/Form1.cs
@@ -12,9 +12,79 @@ namespace YatzyGame
 {
     public partial class Form1 : Form
     {
+        private List<Dice> dices = new List<Dice>
+        {
+            new Dice(),
+            new Dice(),
+            new Dice(),
+            new Dice(),
+            new Dice()
+        };
+        private List<PictureBox> pictureBoxes = new List<PictureBox>();
+        private int rolls;
+        private Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+            InitializeGame();
+
+          
+        }
+        private void InitializeGame()
+        {
+            pictureBoxes.Add(dice1);
+            pictureBoxes.Add(dice2);
+            pictureBoxes.Add(dice3);
+            pictureBoxes.Add(dice4);
+            pictureBoxes.Add(dice5);
+        }
+        private void SetDiceImage(int value,PictureBox box)
+        {
+            switch (value)
+            {
+                case 1:
+                    box.Image = Properties.Resources.dice_1;
+                    break;
+                case 2:
+                    box.Image = Properties.Resources.dice_2;
+                    break;
+                case 3:
+                    box.Image = Properties.Resources.dice_3;
+                    break;
+                case 4:
+                    box.Image = Properties.Resources.dice_4;
+                    break;
+                case 5:
+                    box.Image = Properties.Resources.dice_5;
+                    break;
+                case 6:
+                    box.Image = Properties.Resources.dice_6;
+                    break;
+
+            }
+            box.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        private void RollDice()
+        {
+            for(int i = 0; i< dices.Count; i++)
+            {
+                if (!dices[i].hold)
+                {
+                    dices[i].value = rand.Next(6) + 1;
+                    SetDiceImage(dices[i].value, pictureBoxes[i]);
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            RollDice();
         }
     }
 }
